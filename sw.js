4503452388d95dcec7bcaa14fb898d068b0c92f2
gerodrigueza11 <138688699+gerodrigueza11@users.noreply.github.com>
2025-02-02 20:07:06 -0400

    sw.js

--- sw.js
+++ sw.js
@@ -1,17 +1,14 @@
-// Nombre del cache
 const CACHE_NAME = 'dosis-medicamentos-v1';
 
-// Lista de recursos para cachear
 const ASSETS_TO_CACHE = [
-  '/', // Página principal
-  '/index.html', // Archivo HTML principal
-  '/styles.css', // Archivo CSS (si lo tienes separado)
-  '/script.js', // Archivo JavaScript (si lo tienes separado)
-  '/ico-192x192.png', // Ícono de 192x192
-  '/ico-512x512.png', // Ícono de 512x512
+  '/', 
+  '/index.html', 
+  
+  '/script.js',
+  '/ico-192x192.png',
+  '/ico-512x512.png',
 ];
 
-// Evento de instalación: Cachea los recursos estáticos
 self.addEventListener('install', (event) => {
   console.log('Service Worker instalado');
   event.waitUntil(
@@ -26,7 +23,6 @@ self.addEventListener('install', (event) => {
   );
 });
 
-// Evento de activación: Elimina caches antiguos
 self.addEventListener('activate', (event) => {
   console.log('Service Worker activado');
   event.waitUntil(
@@ -43,24 +39,20 @@ self.addEventListener('activate', (event) => {
   );
 });
 
-// Evento de fetch: Sirve recursos desde el cache o la red
 self.addEventListener('fetch', (event) => {
   console.log('Solicitud interceptada:', event.request.url);
   event.respondWith(
     caches.match(event.request)
       .then((response) => {
-        // Si el recurso está en el cache, lo devuelve
         if (response) {
           console.log('Recurso encontrado en el cache:', event.request.url);
           return response;
         }
-        // Si no está en el cache, lo solicita a la red
         console.log('Recurso no encontrado en el cache, solicitando a la red:', event.request.url);
         return fetch(event.request);
       })
       .catch((error) => {
         console.error('Error al manejar la solicitud:', error);
-        // Puedes devolver una página de respaldo o un mensaje personalizado
         return new Response('No se pudo cargar el recurso. Verifica tu conexión a Internet.');
       })
   );
